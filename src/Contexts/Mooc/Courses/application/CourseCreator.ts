f50e2778d6190f99a2075ceef82bb9bb1baec664
pipe-dev <andres.ospina12@utp.edu.co>
2023-01-01 21:06:52 -0500

    feature: first commit

--- src/Contexts/Mooc/Courses/application/CourseCreator.ts
+++ src/Contexts/Mooc/Courses/application/CourseCreator.ts
@@ -2,15 +2,16 @@ import { Course } from '../domain/Course';
 import { CourseRepository } from '../domain/CourseRepository';
 
 export class CourseCreator {
-  private repository: CourseRepository;
+  private _repository: CourseRepository;
 
   constructor(repository: CourseRepository) {
-    this.repository = repository;
+    this._repository = repository;
   }
 
   async run(id: string, name: string, duration: string): Promise<void> {
+    
     const course = new Course({ id, name, duration });
 
-    return this.repository.save(course);
+    return await this._repository.save(course);
   }
 }
