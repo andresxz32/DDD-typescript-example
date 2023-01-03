206e0e5d7db89a7d24899ca839f19f948a97cae8
pipe-dev <andres.ospina12@utp.edu.co>
2023-01-02 21:21:25 -0500

    feature: add new use case to feature of create course

--- tests/Contexts/Mooc/Courses/__mocks__/CourseRepositoryMock.ts
+++ tests/Contexts/Mooc/Courses/__mocks__/CourseRepositoryMock.ts
@@ -2,14 +2,14 @@ import { CourseRepository } from '../../../../../src/Contexts/Mooc/Courses/domai
 import { Course } from '../../../../../src/Contexts/Mooc/Courses/domain/Course';
 
 export class CourseRepositoryMock implements CourseRepository {
-  private mockSave = jest.fn();
+  private _mockSave = jest.fn();
 
   async save(course: Course): Promise<void> {
-    this.mockSave(course);
+    this._mockSave(course);
   }
 
   assertLastSavedCourseIs(expected: Course): void {
-    const mock = this.mockSave.mock;
+    const mock = this._mockSave.mock;
     const lastSavedCourse = mock.calls[mock.calls.length - 1][0] as Course;
     expect(lastSavedCourse).toBeInstanceOf(Course);
     expect(lastSavedCourse.id).toEqual(expected.id);
